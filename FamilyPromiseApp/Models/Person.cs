64d7faaf42754e123fbeaf1c97bc19d9cc052aa0
Riley Skief <skiefri@hendrix.edu>
2023-12-06 21:52:19 -0600

    Intake Creation

--- FamilyPromiseApp/Models/Person.cs
+++ FamilyPromiseApp/Models/Person.cs
@@ -26,41 +26,73 @@ namespace FamilyPromiseApp.Models
         [Column("FirstName")]
         [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
+        [Display(Name = "Marital Status")]
+        public string MaritalStatus { get; set; }
+        
+        [Display(Name = "Age")]
         public int Age { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        
+        
+        [Display(Name = "Date Admitted")]
+        public int DateAdmitted { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        
+        
+        [Display(Name = "CompletionDate")]
+        public int CompletionDate { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        
+        
         [Display(Name = "Date of Birth")]
         public DateTime DateOfBirth { get; set; }
+        
         [Display(Name = "Cell Number")]
         public string CellPhone { get; set; }
+
         [Display(Name = "Work Number")]
         public string WorkPhone { get; set; }
+
         [Display(Name = "Email Address")]
         public string Email { get; set;}
+        
         [Display(Name = "Are there any additional adults in the household?")]
         public AnyHouseHold? HouseHoldMemberExist { get; set; }
+
         [Display(Name = "Number of additional adults in household")]
         public int HouseHoldNum { get; set; }
+
         [Display(Name = "Name")]
         public string AdultName { get; set; }
+
         [Display(Name = "Age")]
         public int AdultAge { get; set; }
+
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "Date of Birth")]
         public DateTime AdultDateOfBirth { get; set; }
+
         [Display(Name = "Cell Phone")]
         public string AdultCellPhone { get; set; }
+
         [Display(Name = "Work Phone")]
         public string AdultWorkPhone { get; set; }
         [Display(Name = "Email")]
         public string AdultEmail { get; set; }
+        
         [Display(Name = "Are there any additional children in the household?")]
         public AnyChildren? HouseHoldChildrenExist { get; set; }
+
         [Display(Name = "Number of children in household")]
         public int ChildNum { get; set; }
+
         [Display(Name = "Name")]
         public string ChildName { get; set; }
+
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "Date of Birth")]
--- FamilyPromiseApp/Pages/Privacy.cshtml.cs
+++ FamilyPromiseApp/Pages/Privacy.cshtml.cs
@@ -1,19 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using FamilyPromiseApp.Data;
+using FamilyPromiseApp.Models;
 
-namespace FamilyPromiseApp.Pages;
-
-public class PrivacyModel : PageModel
+namespace FamilyPromiseApp.Pages.Intake
 {
-    private readonly ILogger<PrivacyModel> _logger;
-
-    public PrivacyModel(ILogger<PrivacyModel> logger)
+    public class CreateModel2 : PageModel
     {
-        _logger = logger;
-    }
+        private readonly FamilyPromiseApp.Data.FamilyPContext _context;
 
-    public void OnGet()
-    {
+        public CreateModel2(FamilyPromiseApp.Data.FamilyPContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Person Person { get; set; }
+
+        [BindProperty]
+        public Case Case { get; set; }
+
+        // [BindProperty]
+        // public HouseHold NewCase { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Person.Add(Person);
+            _context.Case.Add(Case);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
     }
 }
-
