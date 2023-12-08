7498910cf006616d5eb58f420cd60b9bd405c70d
Riley Skief <skiefri@hendrix.edu>
2023-12-07 21:21:23 -0600

    moved intake form

---
+++ FamilyPromiseApp/Pages/Intake/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FamilyPromiseApp.Data;
+using FamilyPromiseApp.Models;
+
+namespace FamilyPromiseApp.Intake
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly FamilyPromiseApp.Data.FamilyPContext _context;
+
+        public DeleteModel(FamilyPromiseApp.Data.FamilyPContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+      public Person Person { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Persons == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.Persons.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Person = person;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Persons == null)
+            {
+                return NotFound();
+            }
+            var person = await _context.Persons.FindAsync(id);
+
+            if (person != null)
+            {
+                Person = person;
+                _context.Persons.Remove(Person);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ FamilyPromiseApp/Pages/Intake/Details.cshtml.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FamilyPromiseApp.Data;
+using FamilyPromiseApp.Models;
+
+namespace FamilyPromiseApp.Intake
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly FamilyPromiseApp.Data.FamilyPContext _context;
+
+        public DetailsModel(FamilyPromiseApp.Data.FamilyPContext context)
+        {
+            _context = context;
+        }
+
+      public Person Person { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Persons == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.Persons.FirstOrDefaultAsync(m => m.ID == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Person = person;
+            }
+            return Page();
+        }
+    }
+}
