2d6ba9de1301448cb31b9b74ea13e466c02085e8
mason-pAoLo <masonmillionaire725@gmail.com>
2024-04-24 15:59:25 -0500

    Clients page fixed via migration

---
+++ FamilyPromiseApp/Migrations/20240424205825_MinorUpdate.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FamilyPromiseApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class MinorUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName1",
+                table: "IntakeModel");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Person",
+                newName: "First Name");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SSN",
+                table: "IntakeModel",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Reason",
+                table: "IntakeModel",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IDNum",
+                table: "IntakeModel",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "First Name",
+                table: "Person",
+                newName: "FirstName");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SSN",
+                table: "IntakeModel",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Reason",
+                table: "IntakeModel",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "IDNum",
+                table: "IntakeModel",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName1",
+                table: "IntakeModel",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
